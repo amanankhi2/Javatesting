2fd3e4d4d6ca967ffd3cf757718c6b8b2a6c36cd
Amandeep Guru <amanankhi2@gmail.com>
2020-12-14 04:16:19 -0500

    Varibales and Data Types Basics

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+const defaultValue = 0;
+let currentResult = defaultValue ;
+currentResult = currentResult + 10;
+
+let calculationDesc = `(${defaultValue} + 10 )`;
+
+outputResult(currentResult, calculationDesc);
