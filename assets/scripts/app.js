04403fb0673132846d536661329ef7f447d2f66b
Amandeep Guru <amanankhi2@gmail.com>
2020-12-15 05:05:07 -0500

    Added Basic functions like add, sub...

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,42 @@
 const defaultValue = 0;
 let currentResult = defaultValue ;
-currentResult = currentResult + 10;
 
-let calculationDesc = `(${defaultValue} + 10 )`;
+//  Convert user Input to number
+function getUserInput(){
+    return parseInt(userInput.value);
+}
 
-outputResult(currentResult, calculationDesc);
+// Create formatted results
+function createResults(operator,initialResult,enteredNumber){
+    const calcDesc = `${initialResult} ${operator} ${enteredNumber}`;
+    outputResult(currentResult, calcDesc); // function from vendor file.
+}
+
+function add(){
+    const oldresult = currentResult;
+    currentResult = currentResult + getUserInput();
+    createResults('+',oldresult,getUserInput());
+}
+
+function subtract(){
+    const oldresult = currentResult;
+    currentResult = currentResult - getUserInput();
+    createResults('-',oldresult,getUserInput());
+}
+
+function multiply(){
+    const oldresult = currentResult;
+    currentResult = currentResult * getUserInput();
+    createResults('*',oldresult,getUserInput());
+}
+
+function divide(){
+    const oldresult = currentResult;
+    currentResult = currentResult / getUserInput();
+    createResults('/',oldresult,getUserInput());
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
