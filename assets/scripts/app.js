8534f23f03b7e18e86b6452fd3f52183d1efdb97
Amandeep Guru <amanankhi2@gmail.com>
2020-12-16 05:19:01 -0500

    Array and Objects, defer method to load script earlier

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,27 @@
 const defaultValue = 0;
 let currentResult = defaultValue ;
+let userInputLogs = [];
 
 //  Convert user Input to number
 function getUserInput(){
     return parseInt(userInput.value);
+}
+
+// function to create logs
+function writeToLog (
+    operation,
+    prevResult,
+    operationNumber,
+    newResult
+){
+    const userInputLog = {
+        operation: operation,
+        oldresult:prevResult,
+        currentInput: operationNumber,
+        result:newResult
+    };
+    userInputLogs.push(userInputLog);
+    console.log(userInputLogs);
 }
 
 // Create formatted results
@@ -14,26 +32,34 @@ function createResults(operator,initialResult,enteredNumber){
 
 function add(){
     const oldresult = currentResult;
-    currentResult = currentResult + getUserInput();
-    createResults('+',oldresult,getUserInput());
+    const currentInput = getUserInput();
+    currentResult = currentResult + currentInput;
+    createResults('+',oldresult,currentInput);
+    writeToLog('Add',oldresult,currentInput,currentResult);
 }
 
 function subtract(){
     const oldresult = currentResult;
-    currentResult = currentResult - getUserInput();
-    createResults('-',oldresult,getUserInput());
+    const currentInput = getUserInput();
+    currentResult = currentResult - currentInput;
+    createResults('-',oldresult,currentInput);
+    writeToLog('Subtract',oldresult,currentInput,currentResult);
 }
 
 function multiply(){
     const oldresult = currentResult;
-    currentResult = currentResult * getUserInput();
-    createResults('*',oldresult,getUserInput());
+    const currentInput = getUserInput();
+    currentResult = currentResult * currentInput;
+    createResults('*',oldresult,currentInput);
+    writeToLog('Multiply',oldresult,currentInput,currentResult);
 }
 
 function divide(){
     const oldresult = currentResult;
-    currentResult = currentResult / getUserInput();
-    createResults('/',oldresult,getUserInput());
+    const currentInput = getUserInput();
+    currentResult = currentResult / currentInput;
+    createResults('/',oldresult,currentInput);
+    writeToLog('Divide',oldresult,currentInput,currentResult);
 }
 
 addBtn.addEventListener('click', add);
